7ed76bb4445e50e54df04d459d95e3659ce858fc
nagendrabitbucket <nagendra.avr@gmail.com>
2024-01-07 16:40:37 +1100

    prime Number

---
+++ PrimeNumber.py
@@ -0,0 +1,15 @@
+num=27
+flag=false
+if num==1
+ print(num,"is not a prime number")
+elif num>1 
+    for(i in range(2,num))
+        if(num%i==0)
+            flag=True
+            break
+        if flag:
+            print("{0} is not a prime number".num)
+        else:
+            print("{0} is a prime number".num)
+	
+		
